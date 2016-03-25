ca8f60435ab3aa605e3353c8369d4f715baf1082
Brianne Hostutler <briannehostutler@gmail.com>
2016-03-24 20:36:05 -0400

    added google maps

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
  $(document).ready(function(){
       $('.parallax').parallax();
-    });
+});
+
+
+	var map;
+	function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: 40.397, lng: -74.644},
+	  zoom: 8
+	});
+	} 
+
+ // AIzaSyCSVZ6NLbN56qrReRGN7czA9m9y3CrMmjs
