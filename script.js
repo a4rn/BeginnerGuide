546a65af6d244e4709ef80e72144b3bf2a868123
Brianne Hostutler <briannehostutler@gmail.com>
2016-03-24 21:29:25 -0400

    map with marker

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
  $(document).ready(function(){
-      $('.parallax').parallax();
+ 	// loads the parallax effect when the page loads
+      $('.parallax').parallax(); 
 });
 
 
-	var map;
-	function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: 40.397, lng: -74.644},
-	  zoom: 8
-	});
-	} 
+      function initMap() {
+        var rcb = {lat: 40.7282, lng: -74.0776};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: rcb
+        });
 
- // AIzaSyCSVZ6NLbN56qrReRGN7czA9m9y3CrMmjs
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">RCB</h1>'+
+            '<div id="bodyContent">'+
+            '<p>Rutgers Coding Bootcamp</p>'+
+            '<p><a href="https://codingbootcamp.rutgers.edu/">'+
+            'https://codingbootcamp.rutgers.edu/</a> '+
+            '</p>'+
+            '</div>'+
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        var marker = new google.maps.Marker({
+          position: rcb,
+          map: map,
+          title: 'Rutgers Coding Bootcamp'
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+      }
+
+ // google maps API key= AIzaSyCSVZ6NLbN56qrReRGN7czA9m9y3CrMmjs
